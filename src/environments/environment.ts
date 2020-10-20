933c1f0aa165d1e95ede0d2949d1e57216e416fa
IgorMendes <igorsmendes2803@gmail.com>
2020-10-20 20:43:38 -0300

    Criação do app para ler QR code
    App com a função para ler QR code armazenar o historico em um banco de dados online atravez do Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAbaSID5kRAfTTQA6PApOYmrcl64aRTWTQ",
+    authDomain: "leitorqrcodefirebase-4dd39.firebaseapp.com",
+    databaseURL: "https://leitorqrcodefirebase-4dd39.firebaseio.com",
+    projectId: "leitorqrcodefirebase-4dd39",
+    storageBucket: "leitorqrcodefirebase-4dd39.appspot.com",
+    messagingSenderId: "604754988398",
+    appId: "1:604754988398:web:5eb03fd528f7804abfc57f",
+    measurementId: "G-3CFHCZ374E"
+  }
 };
 
 /*
